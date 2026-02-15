05c2956ce330e65c693666a706e2b7eda2d56e63
NotMyRealGitt <stefan.kurcan22@gmail.com>
2026-02-15 19:18:15 +0100

    Add basic skeleton comments.

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@ all: producer
 
 producer: ${SRC_PRODUCER}
 	echo "Building producer"
-	cc ${SRC_PRODUCER} -o ${BINDIR}/producer
+	mkdir -p ${BINDIR}
+	cc ${SRC_PRODUCER} -o ${BINDIR}producer `pkg-config --cflags --libs gstreamer-1.0`
 
 clean:
 	echo "Cleaning all"
--- producer/producer.c
+++ producer/producer.c
@@ -1,9 +1,34 @@
 #include "stdio.h"
+#include <gst/gst.h>
+
+// STFN TODO:
+// Take a look at pipeline examples:
+// https://gist.github.com/hum4n0id/cda96fb07a34300cdb2c0e314c14df0a
+//
+// note:
+// What's `gst-discoverer-1.0`?
 
 int main(int argc, char** argv) {
+  g_print("Starting producer...\n");
 
-  printf("HELLO!\n");
+  // generic setup
+  GstStateChangeReturn ret;
+  GstElement *pipeline, *source, *sink;
+  GstBus *bus;
+  GstMessage *msg;
+  gst_init (&argc, &argv);
 
+  // setup
+  source = gst_element_factory_make ("videotestsrc", "source");
+  pipeline = gst_pipeline_new ("test-pipeline");
+
+  // read file
+
+  // pre-loop
+
+  // streaming loop
+
+  // cleanup
 
   return 0;
 }
